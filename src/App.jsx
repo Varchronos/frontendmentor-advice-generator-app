4fe5f0f4366aba33ad9009d9c791fd3a04482baa
Varchronos <lcs2020062@iiitl.ac.in>
2023-11-19 15:53:02 +0530

    latest draft #1

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useEffect, useState } from 'react'
 import './App.css'
+import dice from './assets/icon-dice.svg'
+import divider from './assets/pattern-divider-desktop.svg'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const [advice, setAdvice] = useState('');
+  const [adviceNumber, setAdviceNumber] = useState(null)
+  const buttonHandeler = async()=>{
+    const randomNum = Math.floor(Math.random() * (100 - 1) + 1);
+    console.log(randomNum)
+    const response = await fetch('https://api.adviceslip.com/advice/'+randomNum);
+    const {slip:data} = await response.json()
+    setAdviceNumber(randomNum)
+    setAdvice(data.advice)
+    console.log(data.advice)
+  }
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+
+      <div className="container">
+        <main>
+        <p>ADVICE #{adviceNumber?adviceNumber:''}</p>
+        <h1>{advice===''?'click to get advice':advice}</h1>
+        <div className="container_foot">
+          <img src={divider} alt="" />
+        </div>
+        </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      <button type='button' className='round_button' onClick={buttonHandeler}>
+          <img src={dice} alt="Dice" />
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
